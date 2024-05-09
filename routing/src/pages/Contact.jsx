ff393be29e25a83c9895af8a373da026352d3e05
Haris Nasir <hafizharis16299@gmail.com>
2024-05-09 09:47:27 -0700

    "Handle delete functionality done"

--- routing/src/pages/Contact.jsx
+++ routing/src/pages/Contact.jsx
@@ -1,33 +1,51 @@
+
 import React, { useEffect, useState } from "react";
-import { ToastContainer, toast } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
-let contacts=[];
+
 function Contact() {
   const [contact, setContact] = useState({
     fullname: '',
     email: '',
   });
+  
   const [contacts,setContacts]=useState([]);
-  const notify = () => toast("Contact has been saved!");
   const handleSubmit = (e) => {
     e.preventDefault();
-   contacts.push(contact);
+
+    let newContact={
+      ...contact,
+      id:Date.now()
+    }
+   contacts.push(newContact);
     localStorage.setItem('Contacts Details', JSON.stringify(contacts));
-    notify();
+
     setContact({
       fullname: '',
-      email: ''
+    email: ''
     })
   };
 
-  useEffect(()=>{
-let storedcontacts=localStorage.getItem('Contacts Details')
-storedcontacts=JSON.parse(storedcontacts)
-if(storedcontacts!=null){
-setContacts(storedcontacts)
-}
-  },[])
  
+useEffect(()=>{
+  let storedContacts=localStorage.getItem('Contacts Details')
+  storedContacts=JSON.parse(storedContacts);
+  if(storedContacts!=null)
+    {
+      setContacts(storedContacts);
+    }
+},[])
+
+const handleDelete = (id)=>{
+
+let contactArr=JSON.parse(localStorage.getItem('Contacts Details'))
+const filteredArr=contactArr.filter((c)=>c.id!=id)
+
+localStorage.setItem('Contacts Details', JSON.stringify(filteredArr));
+location.reload();
+}
+
+
+
+
   return (
     <div className="container">
       <form className="row g-3 py-5 w-50" onSubmit={handleSubmit}>
@@ -35,12 +53,8 @@ setContacts(storedcontacts)
           <label htmlFor="fullName" className="form-label">
             Full Name
           </label>
-          <input
-            type="text"
-            onChange={(e) => {
-              setContact({ ...contact, fullname: e.target.value }); 
-            }}
-        
+          <input type="text" onChange={(e) => {setContact({ ...contact, fullname: e.target.value }); }}
+        value={contact.fullname}
             className="form-control"
             id="fullName"
           />
@@ -54,7 +68,7 @@ setContacts(storedcontacts)
             onChange={(e) => {
               setContact({ ...contact, email: e.target.value });
             }}
-            
+            value={contact.email}
             className="form-control"
             id="email"
           />
@@ -76,28 +90,25 @@ setContacts(storedcontacts)
       <th scope="col">Actions</th>
     </tr>
   </thead>
-  
   <tbody>
   {
-    contacts.length<1?<tr><td><h1>No data found...</h1></td></tr>:
-    contacts.map((c,idx)=><tr key={idx}>
-      <th scope="row">{idx+1}</th>
-      <td>{c.fullname}</td>
-      <td>{c.email}</td>
-      <td>
-        <button className='btn btn-danger me-2'><i className="bi bi-trash"></i></button>
-        <button className='btn btn-warning me-2'><i className="bi bi-pencil-square"></i></button>
-        <button className='btn btn-success me-2'><i className="bi bi-telephone-forward"></i></button>
-        
-      </td>
-    </tr>)
-      
+    contacts.length < 1? <tr><td><h1>No data found...</h1></td></tr>:
+   contacts.map((c,idx)=>   <tr key={idx}>
+    <th scope="row">{idx+1}</th>
+    <td>{c.fullname}</td>
+    <td>{c.email}</td>
+    <td>
+    <button className='btn btn-danger me-2' onClick={()=>handleDelete(c.id)}><i className="bi bi-trash"></i></button>
+    <button className='btn btn-warning me-2'><i className="bi bi-pencil-square"></i></button>
+    <button className='btn btn-success me-2'><i className="bi bi-telephone-forward"></i></button>
+    </td>
+  </tr>)
   }
+   
   </tbody>
 </table>
 
     </div>
   );
 }
-<ToastContainer />
 export default Contact;
